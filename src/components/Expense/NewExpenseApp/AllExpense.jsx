f30d8bb5a66bf9ffb9ae32fb33c5b246a6c2a1c1
theconfusedhead <theconfusedhead7@gmail.com>
2024-07-17 01:43:14 +0530

    update the UI

--- src/components/Expense/NewExpenseApp/AllExpense.jsx
+++ src/components/Expense/NewExpenseApp/AllExpense.jsx
@@ -1,35 +1,53 @@
 import React from "react";
-
+import { format } from "date-fns";
 const AllExpense = ({ allExpense, deleteExpense }) => {
   const handleDelete = (id) => {
     console.log(id);
     deleteExpense(id);
   };
   const showExpense = allExpense.map((expense, index) => {
+    const formattedDate = format(new Date(expense.date), "dd-MMM-yyyy");
     return (
       <div
         key={expense.id}
-        className="d-flex align-items-center justify-content-between bg-light p-2 mb-2 rounded"
+        className="row align-items-center bg-light p-2 mb-2 rounded"
       >
-        <span className="me-2">{`${index + 1})`}</span>
-        <span className="flex-grow-1 me-2">
+        <div className="col-1">{`${index + 1})`}</div>
+        <div className="col-4">
           {`${expense.expenseName
             .charAt(0)
             .toUpperCase()}${expense.expenseName.slice(1)}`}
-        </span>
-        <span className="me-2">{`${expense.amount} ${expense.date}`}</span>
-        <button
-          onClick={() => handleDelete(expense.id)}
-          className="btn btn-secondary btn-sm"
-        >
-          Delete
-        </button>
+        </div>
+        <div className="col-3">{expense.amount}</div>
+        <div className="col-3">{formattedDate}</div>
+        <div className="col-1">
+          <button
+            onClick={() => handleDelete(expense.id)}
+            className="btn btn-secondary btn-sm"
+          >
+            Delete
+          </button>
+        </div>
       </div>
     );
   });
   return (
     <div className="mt-5">
       <h4>Your Expenses:</h4>
+      <div className="row align-items-center bg-light p-2 mb-2 rounded">
+        <div className="col-1">
+          <h3>S No</h3>
+        </div>
+        <div className="col-4">
+          <h3>Expense Name</h3>
+        </div>
+        <div className="col-3">
+          <h3>Expense Amount</h3>
+        </div>
+        <div className="col-4">
+          <h3>Date</h3>
+        </div>
+      </div>
       <div>{showExpense}</div>
     </div>
   );
