5ef0f41ac7b9ee4c9ae74075fcfccd50bd9ac39d
theconfusedhead <theconfusedhead7@gmail.com>
2024-07-17 11:30:38 +0530

    remove style

--- src/components/Expense/NewExpenseApp/AllExpense.jsx
+++ src/components/Expense/NewExpenseApp/AllExpense.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { format } from "date-fns";
-import "./style.css";
+
 const AllExpense = ({ allExpense, deleteExpense }) => {
   const handleDelete = (id) => {
     console.log(id);
